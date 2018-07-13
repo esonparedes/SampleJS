2a456a4a1b496ba595732d1e4fe2d14865eaadae
Jason Paredes <JasonParedes@Deltek.com>
2018-07-13 14:55:24 +0800

    add day

--- demoQueryString.js
+++ demoQueryString.js
@@ -5,6 +5,6 @@ var url = require('url');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
+    var txt = q.year + "/" + q.month + "/" + q.day;
     res.end(txt); //end the response
 }).listen(8090);
